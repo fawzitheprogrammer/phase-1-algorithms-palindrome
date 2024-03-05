5209f42315f6586211f1cc54db76251282ec077d
Fawzi Gharib <fawz11gharib@gmail.com>
2024-03-05 20:19:30 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  
+
+  let lowerCaseWord = word.toLowerCase();
+  let splittedWord = lowerCaseWord.split('');
+  let reversedWord = splittedWord.reverse();
+   
+  let fullyReversedWordJoined = reversedWord.join('');
+
+   if(fullyReversedWordJoined==lowerCaseWord){
+    return true;
+   }
+   else{
+    return false;
+   }
 }
 
-/* 
-  Add your pseudocode here
-*/
 
-/*
-  Add written explanation of your solution here
-*/
+isPalindrome('Fawzi');
 
-// You can run `node index.js` to view these console logs
+
+
+
+
+
+
+// /* 
+//   Add your pseudocode here
+// */
+
+// /*
+//   Add written explanation of your solution here
+// */
+
+//You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("Fawzi"));
 
   console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome("Maram"));
 }
 
 module.exports = isPalindrome;
